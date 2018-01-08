3fd78d70ce4b2a4c7c6a02ba48fba8218246429f
mpaarating <alexmichael.lewis@gmail.com@gmail.com>
2018-01-08 07:45:57 -0500

    first commit

---
+++ tictactoe/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from .views import welcome
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^player/', include('player.urls')),
+    url(r'^welcome$', welcome)
+]
